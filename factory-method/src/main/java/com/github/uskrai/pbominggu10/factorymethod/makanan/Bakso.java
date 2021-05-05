fd77fc14875d6561e8ed6197141654badc3fb451
firda0802 <71951046+firda0802@users.noreply.github.com>
2021-05-05 10:54:32 +0700

    Add Bakso.java

---
+++ factory-method/src/main/java/com/github/uskrai/pbominggu10/factorymethod/makanan/Bakso.java
@@ -0,0 +1,10 @@
+package com.github.uskrai.pbominggu10.factorymethod.makanan;
+
+public class Bakso.java{
+    private Sring namaMakanan = Bakso.class.getSimpleName();
+
+    @Override
+    public void rasa(){
+        System.out.printl(namaMakanan+" : Rasa kuah dan daging yang kenyal menjadikan makanan favorite orang Indonesia")
+        }
+}
