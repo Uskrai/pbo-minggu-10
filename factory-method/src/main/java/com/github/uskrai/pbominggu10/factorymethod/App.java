1a0ae306bd9fac1ca35699f480ef9dc203b26ded
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-05-05 12:41:47 +0800

    Fix Thing

--- factory-method/src/main/java/com/github/uskrai/pbominggu10/factorymethod/App.java
+++ factory-method/src/main/java/com/github/uskrai/pbominggu10/factorymethod/App.java
@@ -4,16 +4,14 @@ import com.github.uskrai.pbominggu10.factorymethod.makanan.JenisMakanan;
 import com.github.uskrai.pbominggu10.factorymethod.makanan.Makanan;
 import com.github.uskrai.pbominggu10.factorymethod.makanan.MakananFactory;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        Makanan order = MakananFactory.newMakanan(JenisMakanan.BAKSO);
-        if ( order != null )
-            order.rasa();
+public class App {
+  public static void main(String[] args) {
+    Makanan order = MakananFactory.newMakanan(JenisMakanan.BAKSO);
+    if (order != null)
+      order.rasa();
 
-        order = MakananFactory.newMakanan(JenisMakanan.GORENGAN);
-        if ( order != null )
-            order.rasa();
-    }
+    order = MakananFactory.newMakanan(JenisMakanan.GORENGAN);
+    if (order != null)
+      order.rasa();
+  }
 }
--- factory-method/src/main/java/com/github/uskrai/pbominggu10/factorymethod/makanan/MakananFactory.java
+++ factory-method/src/main/java/com/github/uskrai/pbominggu10/factorymethod/makanan/MakananFactory.java
@@ -2,14 +2,15 @@ package com.github.uskrai.pbominggu10.factorymethod.makanan;
 
 public class MakananFactory {
 
-    public static Makanan newMakanan(JenisMakanan jenisMakanan) {
-        // if else here
-        if ( jenisMakanan == JenisMakanan.BAKSO )
-            return new Bakso();
-        if ( jenisMakanan == JenisMakanan.GORENGAN)
-            return new Gorengan();
-
+  public static Makanan newMakanan(JenisMakanan jenisMakanan) {
+    switch (jenisMakanan) {
+      case BAKSO:
+        return new Bakso();
+      case GORENGAN:
+        return new Gorengan();
+      default:
         return null;
     }
+  }
 
 }
