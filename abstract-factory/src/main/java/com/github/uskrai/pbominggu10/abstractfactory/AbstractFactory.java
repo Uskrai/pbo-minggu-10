96d3c8bb366d1d4de274437d95dbbfe1fd07a3fe
Putriokt <83640551+Putriokt@users.noreply.github.com>
2021-05-06 17:38:23 +0800

    Update AbstractFactory.java

--- abstract-factory/src/main/java/com/github/uskrai/pbominggu10/abstractfactory/AbstractFactory.java
+++ abstract-factory/src/main/java/com/github/uskrai/pbominggu10/abstractfactory/AbstractFactory.java
@@ -1,6 +1,6 @@
 package com.github.uskrai.pbominggu10.abstractfactory;
 
-public abstract class AbstractFactory<Makanan, Minuman> {
+public abstract class AbstractFactory {
     
     abstract Makanan getMakanan(String makanan);
     abstract Minuman getMinuman(String minuman);
